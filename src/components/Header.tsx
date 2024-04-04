8f726bc7c664e09fd9ae04a0ade7a3cdf4d17e9f
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-04-04 10:37:41 +1030

    Update CSS styles and add horizontalBars class to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import './Header.css'
 import { FiAlignJustify } from "react-icons/fi";
 
+
 const Header = () => {
   return (
     <div className='headerContainer' style={{backgroundColor:'#1b1b1b'}}>
       <img src="./src/assets/logo.png" alt='logo' style={{width:100, height:100, borderRadius:20}}/>
-      <FiAlignJustify style={{width:50, height:50, color:'#333333'}} />
+      <FiAlignJustify className='horizontalBars' style={{width:50, height:50}} />
     </div>
   )
 }
